ad87cf69ab88d28fab321b28f7026a8bdc9d4db2
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-03-14 15:05:34 +0100

    Update Esercizio2.cpp
    Risolto problema spazi aggiuntivi non desidierati

--- 2019-03-11/Esercizio2.cpp
+++ 2019-03-11/Esercizio2.cpp
@@ -1,43 +1,51 @@
 #include <iostream>
 using namespace std;
 int main(){
-    
+
     int C[2][4][5];
     bool B[2][4][5];
-    int n, c=0;
-    
+    int n, c=0,f=0;
+
     cout << "start" << endl;
     cin >> n;
-    
-        for(int k = 0; k<4; k++){
-            for(int j = 0; j<2; j++){
-                for(int i = 0; i<5; i++){
-                    B[j][k][i] = false;
-                }
+
+    for(int k = 0; k<4; k++){
+        for(int j = 0; j<2; j++){
+            for(int i = 0; i<5; i++){
+                B[j][k][i] = false;
             }
         }
-    
-        for(int k = 0; k<4  && c<n; k++){
-            for(int j = 0; j<2  && c<n; j++){
-                for(int i = 0; i<5  && c<n ; i++){
-                    cin >> C[j][k][i];
-                    B[j][k][i] = true;
-                    c++;
-                }
-            }
-        }
-        c=0;
-        
+    }
+
+    for(int k = 0; k<4  && c<n; k++){
         for(int j = 0; j<2  && c<n; j++){
-            for(int k = 0; k<4  && c<n; k++){
-                for(int i = 0; i<5  && c<n; i++){
-                    if(B[j][k][i] == true){
-                        cout << C[j][k][i] << " ";
-                        c++;
-                    }
-                }
-                cout << endl;
+            for(int i = 0; i<5  && c<n ; i++){
+                cin >> C[j][k][i];
+                B[j][k][i] = true;
+                c++;
             }
         }
+    }
+    c=0;
+
+    for(int j = 0; j<2  && c<n; j++){
+        for(int k = 0; k<4  && c<n; k++){
+            for(int i = 0; i<5  && c<n; i++){
+                if(B[j][k][i] == true){
+                    cout << C[j][k][i] << " ";
+                    c++;
+                    f++;
+                }
+            }
+            if(f>=5){
+                cout << endl;
+                f=0;
+            }
+
+        }
+    }
+    if(f!=0){
+        cout << endl;
+    }
     cout << "end";
 }
