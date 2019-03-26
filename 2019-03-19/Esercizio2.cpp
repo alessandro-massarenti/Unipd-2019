afe2ebec58f469f88dac71a08a9f2d210af67cec
Alessandro Massarenti <48263116+alessandro-massarenti@users.noreply.github.com>
2019-03-27 00:05:04 +0100

    Creata funzione stampaS
    Aggiunta la funzione stampaS come richiesto nella consegna.

--- 2019-03-19/Esercizio2.cpp
+++ 2019-03-19/Esercizio2.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int stampaS(int *p, int dim3, int dim2, int i,int  count, int elementi){
+    cout << "\nstrato" << i << endl;
+    for(int j = 0; j< dim2 && count<elementi ; j++){
+        cout << "r" << j << ":";
+        for(int k = 0; k< dim3 && count<elementi; k++){
+            cout <<  p[(j*dim3)+(dim2*dim3*i)+k] << " ";
+            count ++;
+        }
+    }
+    cout << endl;
+    return count;    
+}
+
 int main(){
 
 	int n_ele, lim1,lim2, lim3, X[400];
@@ -15,17 +28,8 @@ int main(){
 
     int count = 0;
     
-    
     for(int i = 0; i< lim1 && count<n_ele ; i++){
-        cout << "\nstrato" << i << endl;
-        for(int j = 0; j< lim2 && count<n_ele ; j++){
-            cout << "r" << j << ":";
-            for(int k = 0; k< lim3 && count<n_ele; k++){
-                cout <<  p[(j*lim3)+(lim2*lim3*i)+k] << " ";
-                count ++;
-            }
-            cout << endl;
-        }
+        count = stampaS(p,lim3,lim2,i,count, n_ele);
     }
 	cout<<"end"<<endl;
 }
